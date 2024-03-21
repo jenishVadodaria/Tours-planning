f125e2eb22a0b1df5d3145eb563e437e527b3d9b
jenishVadodaria <jenish.d.v@gmail.com>
2024-03-21 18:32:37 +0530

    env changes

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 const fs = require('fs')
 const Tour = require('../../models/tourModel')
 
-dotenv.config({ path: `./config.env` })
+dotenv.config({ path: `./.env` })
 const DB =
     process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
